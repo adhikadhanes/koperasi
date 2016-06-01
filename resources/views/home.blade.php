1e8ec90274be943705740b6328ca0ea5f37d01ca
adhikadhanes <adhika.dhaneswara@gmail.com>
2016-06-01 15:09:34 +0700

    commit penjualan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.3/Chart.js"></script>
+
 <br>
 <div class="col-md-6">
             <div class="col-md-6 box">
@@ -64,5 +66,74 @@
                     </div>
         </div>
 
+         <div class="col-md-6">
+      <div class="box box-solid bg-teal-gradient">
+            <div class="box-header with-border">
+              <h3 class="box-title">Penjualan</h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body chart-responsive">
+              <canvas id="myChart" ></canvas>
+            </div>
+            <!-- /.box-body -->
+          </div>
+
+          </div>
+
             <div class="clearfix"></div>
+
+<script>
+
+var xAxis = [];
+          var yAxis1 = [];
+
+            @foreach($penjualan as $rats)
+              xAxis.push('{{$rats->created_at->format('H:i:s')}}');
+              yAxis1.push('{{$rats->Jumlah}}');
+                           
+            @endforeach
+
+var ctx = document.getElementById("myChart");
+
+
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: xAxis,
+        datasets: [{
+
+            fill: false,
+            data: yAxis1,
+            lineTension: 0.1,
+                      borderColor: "rgba(45, 178, 213, 1)",
+                      borderCapStyle: 'butt',
+                      borderDash: [],
+                      borderDashOffset: 0.0,
+                      borderJoinStyle: 'miter',
+                      pointBorderColor: "rgba(45, 178, 213, 1)",
+                      pointBackgroundColor: "#fff",
+                      pointBorderWidth: 1,
+                      pointRadius: 5,
+                      label:'Penjualan hari ini',
+                      pointHitRadius: 10,
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+Chart.defaults.global.responsive = true;
+</script>
 @endsection
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,3 +2,5 @@
 <li><a href="{{ url('/penjualans') }}">Penjualan</a></li>
 <li><a href="{{ url('/inventories') }}">Inventory</a></li>
 <li><a href="{{ url('/users') }}">Users</a></li>
+<li><a href="{{ url('/product') }}">Product</a></li>
+<li><a href="{{ url('/mycart') }}">Cart</a></li>
